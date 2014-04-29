693aa95bf228a77ef237535f036c3870882438c9
usuario <usuario@RamnPC>
2014-04-29 09:59:24 +0200

    v.1.5

--- src/mundialf1/Main.java
+++ src/mundialf1/Main.java
@@ -17,6 +17,9 @@ public class Main extends javax.swing.JFrame {
      */
     public Main() {
         initComponents();
+        ModeloTabla modelo = new ModeloTabla();
+        modelo.setDataList(list1);
+        jTable1.setModel(modelo);
     }
 
     /**
@@ -33,6 +36,8 @@ public class Main extends javax.swing.JFrame {
         list1 = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : query1.getResultList();
         jButton1 = new javax.swing.JButton();
         panelF13 = new mundialf1.PanelF1();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -43,23 +48,42 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(452, Short.MAX_VALUE))
-            .addComponent(panelF13, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelF13, javax.swing.GroupLayout.PREFERRED_SIZE, 508, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 498, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelF13, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1))
+                    .addComponent(panelF13, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -123,6 +147,8 @@ public class Main extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.persistence.EntityManager entityManager1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private java.util.List<Mundial> list1;
     private mundialf1.PanelF1 panelF13;
     private javax.persistence.Query query1;
---
+++ src/mundialf1/ModeloTabla.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mundialf1;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author usuario
+ */
+public class ModeloTabla extends AbstractTableModel{
+    
+    List<Mundial> list;
+    public void setDataList(List<Mundial> list){
+        this.list = list;
+    }
+    @Override
+    public int getRowCount() {
+        return list.size();
+        
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+       Mundial mundial = list.get(rowIndex);
+       switch(columnIndex){
+           case 0:
+               return mundial.getNombrePiloto();
+           case 1:
+               return mundial.getFechaNac();
+           case 2: 
+               return mundial.getNacionalidad();
+           case 3:
+               return mundial.getPuntosTotales();
+           case 4:
+               return mundial.getPuntosUltimaCarrera();
+           default:
+               return null;
+       }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return super.getColumnClass(columnIndex); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch(column){
+           case 0:
+               return "Nombre piloto";
+           case 1:
+               return "Fecha Nacimiento";
+           case 2: 
+               return "Nacionalidad";
+           case 3:
+               return "Puntos Totales";
+           case 4:
+               return "Puntúa en la última carrera";
+           default:
+               return "";
+       }
+    }
+    
+}
